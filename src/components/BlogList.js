3af97cbacc1f84e8a8c7d6b0fe182bd0a152ffba
julidiazdev <dev.julidiaz.es@gmail.com>
2021-08-09 19:25:10 +0200

    Fixing sorting order for posts in listing

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -10,7 +10,9 @@ export default function BlogList() {
     // Defining GraphQL query, using the useStaticQuery hook
     const data = useStaticQuery(graphql`
         {
-            allMarkdownRemark {
+            # Here in the GraphQL query we set the correct date order for the posts,
+            # using a parameter (sort), the field used for sort, and sort order
+            allMarkdownRemark (sort: { fields: frontmatter__date, order: DESC }) {
                 edges {
                     node {
                         id
